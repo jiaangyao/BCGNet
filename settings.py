d07025d47ea3bd2f5ee04d35b97dfc936ded69b6
jrmxn <32486819+jrmxn@users.noreply.github.com>
2019-12-31 07:32:37 -0500

    Update settings.py
    I usually save git info along with the data like this - use as needed

--- settings.py
+++ settings.py
@@ -1,6 +1,25 @@
 # Directory structure
 
+class GitLabel():
+    def __init__(self):
+        try:
+            self.label_ = subprocess.check_output(["git", "describe", "--tag"]).strip().decode("utf-8")
+        except:
+            self.label_ = 'vx.x.x'
+            print('Not git tag found! Setting to {}\n'.format(self.label_))
 
+    def __str__(self):
+        return str(self.label_)
+
+    @property
+    def label(self):
+        return self.label_
+
+    @property
+    def tag(self):
+        return self.label_.split('-')[0]
+
+    
 def init(_project_name, _d_root):
     global project_name
     project_name = _project_name  # user forced to specify
@@ -16,13 +35,17 @@ def init(_project_name, _d_root):
     # filtered at 0.1Hz? Downsampled to 500Hz?:
 
     global d_data
-    d_data = d_root / 'proc/ga_rm'  # settable data location (this
+    d_data = d_root / 'proc' / 'ga_rm'  # settable data location (this
     # can be our default, but I guess user should be forced to specify
     # full path)
 
     global d_mne
-    d_mne = d_root / 'proc_bcgnet/mne/'  # data_loader.py output goes here
+    d_mne = d_root / 'proc_bcgnet' / 'mne/'  # data_loader.py output goes here
 
     global d_features
-    d_features = d_root / 'proc_bcgnet/features/$experiment_id$'
+    d_features = d_root / 'proc_bcgnet' / 'features' / '$experiment_id$'
     # feature_extractor.py output goes here
+    
+    global git_label
+    git_label = GitLabel()
+
